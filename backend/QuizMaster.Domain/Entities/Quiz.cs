b3672bb7e071a3958dc9855701503c4c70eb3ea4
AdityaSankar <adityassankar@gmail.com>
2026-02-03 18:51:54 +0530

    feat: add quiz use case in application layer

--- backend/QuizMaster.Domain/Entities/Quiz.cs
+++ backend/QuizMaster.Domain/Entities/Quiz.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 
-public class Quiz
+namespace QuizMaster.Domain.Entities
 {
-	public Guid Id { get; private set; }
-	public GUid UserId { get; private set; }
-	public string Category { get; private set; }
-	public int TotalScore { get; private set; }
-	public Quiz(Guid userId,string category)
-	{
-		Id=Guid.NewGuid();
-		UserId=userId;
-		Category=category;
-		TotalScore=0;
-	}
-	public void TotalScore(int points) 
-	{
-		TotalScore += points;
-	}
+    public class Quiz
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public string Category { get; private set; }
+        public int TotalScore { get; private set; }
+
+        private Quiz() { } // Required by EF Core
+
+        public Quiz(Guid userId, string category)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Category = category;
+            TotalScore = 0;
+        }
+
+        public void AddScore(int points)
+        {
+            TotalScore += points;
+        }
+    }
 }
